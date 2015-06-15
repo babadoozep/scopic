a4ba1bcaf63f7a2e1e1d83dd76f20dd3d59b1adc
babadoozep <pdxmichel@gmail.com>
2015-06-15 14:15:31 -0700

    Add first component, 'App'

---
+++ app/components/app.js
@@ -0,0 +1,14 @@
+var React = require("react")
+
+
+var App = React.createClass({
+
+  render(){
+  return <div>
+  <button>Freelance</button>
+  <button>Hiring</button>
+  </div>
+}
+})
+
+module.exports = App
